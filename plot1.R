a15f79a20b27caff44ef7c7e288146334ddf4bfd
benmcallister <mcallister.ben@gmail.com>
2014-07-13 12:36:32 -0400

    add plot1.R and plot1.png; edit README.md

---
+++ plot1.R
@@ -0,0 +1,16 @@
+## this R script assumes that you have downloaded and unzipped the data from the following link to a folder called "data" in your working directory: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+## read the full dataset
+data <- read.csv("./data/household_power_consumption.txt", header = TRUE, sep = ";", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = "?")
+
+## subset the data for only Feb 1-2 2007
+data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+## convert the class of the Date column to Date
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+
+## create a histogram of Global Active Power
+png(filename = "plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab  = "Global Active Power (kilowatts)")
+dev.off()
