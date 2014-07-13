940c83a3f15bca4854cfb9b1cb6e39bac4693840
benmcallister <mcallister.ben@gmail.com>
2014-07-13 14:29:53 -0400

    add plot3.R and plot3.png

---
+++ plot3.R
@@ -0,0 +1,21 @@
+## this R script assumes that you have downloaded and unzipped the data from the following link to a folder called "data" in your working directory: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+## read the full dataset
+data <- read.csv("./data/household_power_consumption.txt", header = TRUE, sep = ";", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = "?")
+
+## subset the data for only Feb 1-2 2007
+data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+## create a column with date and time
+data$datetime <- paste(data$Date, data$Time)
+data$datetime <- strptime(data$datetime, "%d/%m/%Y %H:%M:%S")
+
+with(data, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+with(data, lines(datetime, Sub_metering_2, col = "red"))
+with(data, lines(datetime, Sub_metering_3, col = "blue"))
+legend("topright", legend = names(data)[7:9], lty= "solid", col = c("black", "red", "blue"))
+
+## create a histogram of Global Active Power
+png(filename = "plot3.png", width = 480, height = 480)
+with(data, plot(datetime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+dev.off()
